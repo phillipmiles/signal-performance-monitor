d5e2979bde2d22af075fc7209b4dce85a7753800
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2021-05-28 00:48:50 +1000

    Continue work on calcing support/resistance

--- src/functions/analysis/calcTrendLines.ts
+++ src/functions/analysis/calcTrendLines.ts
@@ -9,9 +9,15 @@ const findMinMaxs = (marketData, rangeConstant) => {
     range = range + rangeConstant
   ) {
     let rangeHigh = { x: 0, y: 0 };
-    let rangeLow = { x: 0, y: 0 };
+    let rangeLow = { x: range, y: marketData[range].low };
     let rangeBodyHigh = { x: 0, y: 0 };
-    let rangeBodyLow = { x: 0, y: 0 };
+    let rangeBodyLow = {
+      x: range,
+      y:
+        marketData[range].close < marketData[range].open
+          ? marketData[range].close
+          : marketData[range].open,
+    };
 
     for (
       let i = range;
@@ -23,20 +29,20 @@ const findMinMaxs = (marketData, rangeConstant) => {
         rangeHigh = { x: i, y: priceData.high };
       }
 
-      if (priceData.low > rangeLow) {
+      if (priceData.low < rangeLow.y) {
         rangeLow = { x: i, y: priceData.low };
       }
 
       const bodyHigh =
         priceData.close > priceData.open ? priceData.close : priceData.open;
       const bodyLow =
-        priceData.close > priceData.open ? priceData.open : priceData.close;
+        priceData.close < priceData.open ? priceData.open : priceData.close;
 
       if (bodyHigh > rangeBodyHigh.y) {
         rangeBodyHigh = { x: i, y: bodyHigh };
       }
 
-      if (bodyLow > rangeBodyLow.y) {
+      if (bodyLow < rangeBodyLow.y) {
         rangeBodyLow = { x: i, y: bodyLow };
       }
     }
@@ -45,18 +51,56 @@ const findMinMaxs = (marketData, rangeConstant) => {
     rangeBodyHighsArray.push(rangeBodyHigh);
     rangeBodyLowsArray.push(rangeBodyLow);
   }
+  console.log('RL', rangeLowsArray);
   return {
     highs: rangeHighsArray,
     lows: rangeLowsArray,
     bodyHighs: rangeBodyHighsArray,
-    bodyLows: rangeBodyHighsArray,
+    bodyLows: rangeBodyLowsArray,
   };
+};
+
+const findSupportLevels = (coords: any[]) => {
+  // Smallest to largets
+  const sortedCoords = coords.sort((first, second) => first.y - second.y);
+  // const sortedCoords = coords.sort((first, second) => second.y - first.y);
+
+  const supports = [];
+
+  // Loop through the array...
+  for (let i = 0; i < sortedCoords.length; i = 0) {
+    const coordsFoundToBeClose = [sortedCoords[0]];
+    let stillFindingCloseCoords = true;
+
+    sortedCoords.splice(0, 1);
+
+    while (stillFindingCloseCoords && sortedCoords.length > 0) {
+      if (
+        coordsFoundToBeClose[0].y + coordsFoundToBeClose[0].y * 0.013 >
+        sortedCoords[0].y
+      ) {
+        // console.log('p');
+        coordsFoundToBeClose.push(sortedCoords[0]);
+        sortedCoords.splice(0, 1);
+      } else {
+        stillFindingCloseCoords = false;
+      }
+    }
+
+    const sum = coordsFoundToBeClose.reduce((a, b) => a + b.y, 0);
+    const median = sum / coordsFoundToBeClose.length;
+
+    supports.push({ y: median, strength: coordsFoundToBeClose.length });
+  }
+  return supports;
 };
 
 export const calcTrendLines = (marketData: any[]) => {
   const { highs, lows, bodyHighs, bodyLows } = findMinMaxs(marketData, 10);
-  console.log(highs);
-  console.log(lows);
-  console.log(bodyHighs);
-  console.log(bodyLows);
+  const supports = findSupportLevels(lows);
+  console.log('supports', supports);
+  // console.log(highs);
+  // console.log(lows);
+  // console.log(bodyHighs);
+  // console.log(bodyLows);
 };
